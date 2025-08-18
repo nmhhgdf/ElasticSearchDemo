caa0ce558efedb5b0cefa019012d7c8fd5c5bf35
Spiral <120575257@qq.com>
2025-08-18 23:17:35 +0800

    首次提交

---
+++ src/test/java/org/example/elasticsearchdemo/ElasticSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elasticsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
