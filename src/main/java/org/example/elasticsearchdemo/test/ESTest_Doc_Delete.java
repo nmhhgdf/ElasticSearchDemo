ea734c63e92687f2ea5886349f3548f71d1ad216
Spiral <120575257@qq.com>
2025-08-20 22:06:37 +0800

    文档增删改查

---
+++ src/main/java/org/example/elasticsearchdemo/test/ESTest_Doc_Delete.java
@@ -0,0 +1,32 @@
+package org.example.elasticsearchdemo.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+public class ESTest_Doc_Delete {
+
+    public static void main(String[] args) throws IOException {
+
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("192.168.75.129", 9200, "http"))
+        );
+
+        DeleteRequest request = new DeleteRequest();
+        request.index("user").id("1001");
+
+        DeleteResponse response = esClient.delete(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+
+        esClient.close();
+
+    }
+
+}
---
+++ src/main/java/org/example/elasticsearchdemo/test/ESTest_Doc_Update.java
@@ -0,0 +1,35 @@
+package org.example.elasticsearchdemo.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+public class ESTest_Doc_Update {
+
+    public static void main(String[] args) throws IOException {
+
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("192.168.75.129", 9200, "http"))
+        );
+
+        UpdateRequest request = new UpdateRequest();
+        request.index("user").id("1001");
+        request.doc(XContentType.JSON, "sex", "女");
+
+        UpdateResponse response = esClient.update(request, RequestOptions.DEFAULT);
+        System.out.println(response.getResult());
+
+        esClient.close();
+
+    }
+
+}
