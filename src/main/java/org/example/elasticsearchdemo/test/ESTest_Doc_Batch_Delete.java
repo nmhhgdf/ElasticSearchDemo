0e094aeb07a75c6d23540f5a2156d99c8c551e82
Spiral <120575257@qq.com>
2025-08-20 22:31:30 +0800

    批量增加、删除

---
+++ src/main/java/org/example/elasticsearchdemo/test/ESTest_Doc_Batch_Delete.java
@@ -0,0 +1,37 @@
+package org.example.elasticsearchdemo.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+public class ESTest_Doc_Batch_Delete {
+
+    public static void main(String[] args) throws IOException {
+
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("192.168.75.129", 9200, "http"))
+        );
+
+        BulkRequest request = new BulkRequest();
+
+        request.add(new DeleteRequest().index("user").id("1001"));
+        request.add(new DeleteRequest().index("user").id("1002"));
+        request.add(new DeleteRequest().index("user").id("1003"));
+
+        BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        System.out.println(response.getItems());
+
+        esClient.close();
+
+    }
+
+}
