844ec77e5d87211f7dea0accd31f155c60f20c4d
Spiral <120575257@qq.com>
2025-08-24 22:16:09 +0800

    增加Spring-data-elasticsearch

---
+++ es-spring-demo/src/main/java/org/example/es/esspringdemo/SpringDataElasticSearchMainApplication.java
@@ -0,0 +1,13 @@
+package org.example.es.esspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticSearchMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataElasticSearchMainApplication.class, args);
+    }
+
+}
